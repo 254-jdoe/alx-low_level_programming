3831fe51d64c050ace8ea4f2e070d5d835969b82
254-jdoe <99394120+254-jdoe@users.noreply.github.com>
2022-03-24 20:00:51 +0300

    my commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+/**
+ *_strcat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will also be changed
+ *Return: dest
+ */
+
+char *_strcat(char *dest, char *src);
+{
+int i, j;
+
+i = 0;
+while (dest[i] !- '\0')
+{
+i++;
+}
+
+j = 0;
+while (src[j] !=[A[A[A[A[A[A[=[B[B[B[B[B[A[A[A[A[A[D[D[D[D[B[B[B[B[B[B[C[C[C '\0')
+{
+dest[i] = src [j]:
+j++;
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
